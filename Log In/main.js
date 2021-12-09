864c2038dec68ff8dc8eed672b717f27e833747f
mjhdiheb02@gmail.com <mjhdiheb02@gmail.com>
2021-12-09 21:23:56 +0100

    second

--- Log In/main.js
+++ Log In/main.js
@@ -1,3 +1,25 @@
+
+
+$("#desNaruto").hide()
+$(".vid1").hide()
+$(".vid2").hide()
+
+function nar(){
+    $("#AC1").hide()
+    $("#AN2").hide()
+    $("#HR1").hide()
+    $("#HR2").hide()
+    $("#HR3").hide()
+    $("#AN2").hide()
+    $("#AC2").hide()
+    $("#AC3").hide()
+    $(".vid1").show().css("left","584px").css("top","19px")
+    $("#AN1").css("left","664px").css("top","4px")
+    $("#desNaruto").show()
+    
+
+}
+
 function each(coll, f) {
     if (Array.isArray(coll)) {
       for (var i = 0; i < coll.length; i++) {
@@ -49,12 +71,85 @@ var horror2 = movie($(".horr2"))
 var horror3 = movie($(".horr3"))
 var Horrors= [horror1,horror2,horror3]
 //////////////////////////////////////////
+var Movies=[Animes,Actions,Horrors]
 
 function act(){
     var Movies=[Animes,Actions,Horrors]
     return filter(Movies,function(element){
         if(element === Actions){
-            return element 
+            console.log (element) 
         }
     })
+}
+function act(){
+    $(".action1").css("top","3px").css("left","468")
+    $("#AC2").css("top","-415px")
+    $("#AC3").css("left","68px")
+    $(".action3").css("top","-830px")
+    $("#AN1").hide()
+    $("#AN2").hide()
+    $("#HR1").hide()
+    $("#HR2").hide()
+    $("#HR3").hide()
+    $(".bot1").hide()
+
+}
+
+
+function ani(){
+    var Movies=[Animes,Actions,Horrors]
+    return filter(Movies,function(element){
+        if(element === Animes){
+            console.log (element) 
+        }
+    })
+}
+
+
+function hrr(){
+    var Movies=[Animes,Actions,Horrors]
+    return filter(Movies,function(element){
+        if(element === Animes){
+            console.log (element) 
+        }
+    })
+}
+function hrr(){
+    $("#HR1").css("left","46px").css("top","21px")
+    $("#HR2").css("left","719px").css("top","-394px")
+    $("#HR3").css("left","383px").css("top","-812px")
+
+    $("#AN1").hide()
+    $(".bot1").hide()
+    $("#AN2").hide()
+    $("#AC1").hide()
+    $("#AC2").hide()
+    $("#AC3").hide()
+
+}
+
+function ani(){
+    $("#AN2").css("left","456px").css("top","-415px")
+    $("#HR1").hide()
+    $("#HR2").hide()
+    $("#HR3").hide()
+    $("#AC1").hide()
+    $("#AC2").hide()
+    $("#AC3").hide()
+}
+
+function Hand(){
+    $(".bot1").hide()
+    $(".bot2").hide()
+    $("#AN1").hide()
+    $("#HR1").hide()
+    $("#HR2").hide()
+    $("#HR3").hide()
+    $("#AN2").hide()
+    $("#AC2").hide()
+    $("#AC3").hide()
+    $(".vid2").show()
+    $("#AC1").css("left","664px").css("top","4px")
+    
+
 }

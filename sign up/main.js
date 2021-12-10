d4a6b1b1a7e36d93d20f38eca9b53e0ca5ba26bf
mjhdiheb02@gmail.com <mjhdiheb02@gmail.com>
2021-12-10 08:14:42 +0100

    done

--- sign up/main.js
+++ sign up/main.js
@@ -1,6 +1,32 @@
-var x = document.getElementById("#mail").value=12
-$("#sing").click(function(){
-    if(x===111){
-       alert("gooo") 
-    }
+$(".mail").hide()
+$(".pass").hide()
+
+
+var x = document.getElementById("#mail")
+$(".log2").click(function(){
+    window.location.assign("file:///C:/Users/INFOKOM/OneDrive/Desktop/NewFilms/Log%20In/index1.html?")
+
 })
+$("#sing").click(function(){
+    $("#mail").hide()
+    $("#Log").hide()
+
+    $("#sing").hide()
+    $(".log").show()
+    $(".log2").show()
+    $(".mail").show()
+    $(".pass").show()
+})
+
+$(".log").hide()
+function log(){
+    $(".mail").show()
+    $(".pass").show()
+    $("#sing").hide()
+    $("#mail").hide()
+    $(".log").show()
+    $(".log2").show(
+    $("#Log").hide()
+    )
+}
+
